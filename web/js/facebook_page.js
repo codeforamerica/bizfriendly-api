c89c64c89f121194fc0ddfd5b124b62dd524afd1
Andrew Hyder <hyd415@gmail.com>
2013-04-18 21:42:39 -0700

    Debugging safari and firefox.

--- web/js/facebook_page.js
+++ web/js/facebook_page.js
@@ -84,10 +84,10 @@ var fbLoginStatus = function(){
 }
 
 // init the FB JS SDK
-window.fbAsyncInit = function() {
+// window.fbAsyncInit = function() {
   FB.init({
     appId      : '158953284268352', // App ID from the app dashboard
-    // channelUrl : 'channel.html', // Channel file for x-domain comms
+    channelUrl : 'http://ondrae.github.io/howtocity/web/channel.html', // Channel file for x-domain comms
     status     : true,              // Check Facebook Login status
     cookie     : true, // enable cookies to allow the server to access the session
     // xfbml      : true  // parse XFBML
@@ -96,15 +96,15 @@ window.fbAsyncInit = function() {
     console.log('Facebook has loaded.');
   }
   fbLoginStatus();
-}
-// Load the SDK asynchronously
-(function(d, s, id){
-   var js, fjs = d.getElementsByTagName(s)[0];
-   if (d.getElementById(id)) {return;}
-   js = d.createElement(s); js.id = id;
-   js.src = "//connect.facebook.net/en_US/all.js";
-   fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
+
+// // Load the SDK asynchronously
+// (function(d, s, id){
+//    var js, fjs = d.getElementsByTagName(s)[0];
+//    if (d.getElementById(id)) {return;}
+//    js = d.createElement(s); js.id = id;
+//    js.src = "//connect.facebook.net/en_US/all.js";
+//    fjs.parentNode.insertBefore(js, fjs);
+//  }(document, 'script', 'facebook-jssdk'));
 
 
 // Steps Functions ---------------------
