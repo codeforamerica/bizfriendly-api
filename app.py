78631f16f26f455e5e07c3556038c20f73defff6
Andrew Hyder <hyd415@gmail.com>
2013-05-15 18:06:51 -0700

    Playing with Postgres

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import os
 from flask import Flask, render_template, send_from_directory
+from flask.ext.sqlalchemy import SQLAlchemy
 
 #----------------------------------------
 # initialization
 #----------------------------------------
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+db = SQLAlchemy(app)
 
 app.config.update(
     DEBUG = True,
