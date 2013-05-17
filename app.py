54a3bef1387e6dbb272dd971c9b519f6a1724384
Andrew Hyder <hyd415@gmail.com>
2013-05-16 18:18:42 -0700

    Full site structure up. With tests.

--- app.py
+++ app.py
@@ -14,10 +14,11 @@ db = SQLAlchemy(app)
 
 app.config.update(
     DEBUG = True,
+    SQLALCHEMY_DATABASE_URI = 'postgres://hackyourcity@localhost/howtocity'
 )
 
 #----------------------------------------
-# database
+# models
 #----------------------------------------
 
 class Category(db.Model):
@@ -39,20 +40,23 @@ class Lesson(db.Model):
     name = db.Column(db.Unicode, unique=True)
     description = db.Column(db.Unicode)
     url = db.Column(db.Unicode)
-    # category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
 
-    def __init__(self, name, description, url):
+    def __init__(self, name, description, url, category_id):
         self.name = name
         self.description = description
         self.url = url
-        # self.category_id = category_id
+        self.category_id = category_id
+
+    def __repr__(self):
+        return '<Lesson %r>' % self.name
 
 class Step(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Unicode, unique=True)
     description = db.Column(db.Unicode)
     url = db.Column(db.Unicode)
-    # lesson_id = db.Column(db.Integer, db.ForeignKey('lesson.id'))
+    lesson_id = db.Column(db.Integer, db.ForeignKey('lesson.id'))
 
     def __init__(self, name, description, url):
         self.name = name
@@ -60,19 +64,26 @@ class Step(db.Model):
         self.url = url
         # self.lesson_id = lesson_id
 
+    def __repr__(self):
+        return '<Step %r>' % self.name
+
 
 # Create the database tables.
 if app.config['DEBUG']:
+
     db.drop_all()
     db.create_all()
-    promote_online = Category('How to Promote Your Business Online', 'Lessons for promoting your business online.','category/promote_your_business_online')
-    beyond_cash = Category('How to Bring Your Business Beyond Cash Only', 'Lessons for bringing your business beyond cash only.','category/beyond_cash_only')
-    fb_page_lesson = Lesson('How to create Facebook Page', 'Lesson on how to create a Facebook Page.','facebook_page')
-    twitter_lesson = Lesson('twitter_account', 'Lesson to create a Twitter account.','twitter_account')
+    promote_online = Category('How to Promote Your Business Online', 'Lessons for promoting your business online.','promote_your_business_online')
+    beyond_cash = Category('How to Bring Your Business Beyond Cash Only', 'Lessons for bringing your business beyond cash only.','beyond_cash_only')
+    fb_page_lesson = Lesson('How to create Facebook Page', 'Lesson on how to create a Facebook Page.','facebook_page', 1)
+    twitter_lesson = Lesson('How to create a Twitter Account', 'Lesson on how to create a Twitter account.','twitter_account', 1)
 
     db.session.add(promote_online)
+    db.session.commit()
     db.session.add(beyond_cash)
+    db.session.commit()
     db.session.add(fb_page_lesson)
+    db.session.commit()
     db.session.add(twitter_lesson)
     db.session.commit()
 
@@ -83,9 +94,9 @@ manager = flask.ext.restless.APIManager(app, flask_sqlalchemy_db=db)
 
 # Create API endpoints, which will be available at /api/<tablename> by
 # default. Allowed HTTP methods can be specified as well.
-manager.create_api(Category, methods=['GET', 'POST', 'DELETE'])
-manager.create_api(Lesson, methods=['GET', 'POST', 'DELETE'])
-manager.create_api(Step, methods=['GET', 'POST', 'DELETE'])
+manager.create_api(Category, methods=['GET', 'POST', 'DELETE'], url_prefix='/api/v1', collection_name='categories')
+manager.create_api(Lesson, methods=['GET', 'POST', 'DELETE'], url_prefix='/api/v1', collection_name='lessons')
+manager.create_api(Step, methods=['GET', 'POST', 'DELETE'], url_prefix='/api/v1', collection_name='steps')
 
 #----------------------------------------
 # controllers
@@ -103,18 +114,27 @@ def page_not_found(e):
 def index():
     return render_template('index.html')
 
-@app.route("/fb_page_instructions.html")
-def fb_page_instructions():
-    return render_template('fb_page_instructions.html')
-
 @app.route("/categories")
 def categories():
     categories = Category.query.all()
     return render_template('categories.html', categories=categories)
 
-@app.route("/category/<category>")
-def category(category):
-    return render_template(category+'.html')
+@app.route("/category/<category_url>")
+def category(category_url):
+    # Get the correct lessons for this category
+    category = Category.query.filter_by(url=category_url).first()
+    lessons = Lesson.query.filter_by(category_id=category.id).all()
+    return render_template(category_url+'.html', category=category, lessons=lessons, )
+
+@app.route("/category/<category_url>/lesson/<lesson_url>")
+def lesson(category_url, lesson_url):
+    category = Category.query.filter_by(url=category_url).first()
+    lesson = Lesson.query.filter_by(url=lesson_url).first()
+    return render_template(lesson_url+'.html', category=category, lesson=lesson)
+
+@app.route("/category/<category_url>/lesson/<lesson_url>/<instructions_url>")
+def instructions(category_url, lesson_url, instructions_url):
+    return render_template(instructions_url+'.html')
 
 
 #----------------------------------------
--- app_test.py
+++ app_test.py
@@ -10,13 +10,48 @@ class AppTestCase(unittest.TestCase):
 
 	def test_index(self):
 		rv = self.app.get('/')
-		assert '200 OK' in rv.status
-		assert '<script src="/static/js/index.js"></script>' in rv.data
+		
+	def test_categories(self):
+		rv = self.app.get('/categories')
+		assert rv.status_code == 200
 
-	def test_fb(self):
-		rv = self.app.get('/fb_page_instructions.html')
-		assert '200 OK' in rv.status
-		assert '<script src="/static/js/facebook_page.js"></script>' in rv.data
+	def test_category(self):
+		rv = self.app.get('/category/promote_your_business_online')
+		assert rv.status_code == 200
+
+	def test_lesson(self):
+		rv = self.app.get('/category/promote_your_business_online/lesson/facebook_page')
+		assert rv.status_code == 200
+
+	def test_instructions(self):
+		rv = self.app.get('/category/promote_your_business_online/lesson/facebook_page/facebook_page_instructions')
+		assert rv.status_code == 200
+
+	def test_category_model(self):
+		categories = app.Category.query.all()
+		assert isinstance(categories, list)
+		assert categories[0].id
+		assert categories[0].name
+		assert categories[0].description
+		assert categories[0].url
+
+	def test_lesson_model(self):
+		lessons = app.Lesson.query.all()
+		assert isinstance(lessons, list)
+		assert lessons[0].id
+		assert lessons[0].name
+		assert lessons[0].description
+		assert lessons[0].url
+		assert lessons[0].category_id
+
+	# def test_step_model(self):
+	# 	steps = app.Step.query.all()
+	# 	assert isinstance(steps, list)
+	# 	assert steps[0].id
+	# 	assert steps[0].name
+	# 	assert steps[0].description
+	# 	assert steps[0].url
+	# 	assert steps[0].category_id
 
 
 if __name__ == '__main__':
