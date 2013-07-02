d2d5460e1306c6074349794026b919c04660e7b6
Andrew Hyder <hyd415@gmail.com>
2013-07-01 17:13:12 -0700

    Redid app architecture.

--- howtocity/__init__.py
+++ howtocity/__init__.py
@@ -1,13 +1,19 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.heroku import Heroku
+
+#----------------------------------------
+# initialization
+#----------------------------------------
 
 app = Flask(__name__, template_folder='../web/templates', static_folder='../web/static')
+heroku = Heroku(app) # Sets CONFIG automagically
 db = SQLAlchemy(app)
 
-app.config.update(
-	DEBUG = True,
-    SQLALCHEMY_DATABASE_URI = 'postgres://hackyourcity@localhost/howtocity',
-    SECRET_KEY = '123456'
-)
+# app.config.update(
+# 	DEBUG = True,
+# 	SQLALCHEMY_DATABASE_URI = 'postgres://hackyourcity@localhost/howtocity',
+# 	SECRET_KEY = '123456'
+# )
 
 import api, web
