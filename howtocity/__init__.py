20f60601eb7cb7b77ed8c94c4fbb89a4050c1785
Andrew Hyder <hyd415@gmail.com>
2013-07-21 21:34:38 -0700

    Finished facebook. Need to rename the remember column still. Closes #18.

--- howtocity/__init__.py
+++ howtocity/__init__.py
@@ -131,6 +131,27 @@ admin.add_view(StepView(Step, db.session))
 
 # Functions --------------------------------------------------------
 
+def get_data_at_endpoint(json_data, endpoints):
+    for endpoint in endpoints:
+        json_data = json_data[endpoint]
+    data = json_data # Should be a string or int now, not json
+    return data
+
+def boolify(s):
+    if s == 'True' or s == 'true':
+        return True
+    if s == 'False' or s == 'false':
+        return False
+    raise ValueError("huh?")
+
+def autoconvert(s):
+    for fn in (boolify, int, float):
+        try:
+            return fn(s)
+        except ValueError:
+            pass
+    return s
+
 @app.route('/logged_in', methods=['POST'])
 def logged_in():
     # Check if the user is logged into the service
@@ -143,18 +164,13 @@ def logged_in():
         counter = counter + 1
         r = requests.get(trigger_endpoint+access_token)
         rjson = r.json()
-        print rjson
         for trigger_check_endpoint in trigger_check_endpoints:
             try:
                 rjson = rjson[trigger_check_endpoint]
             except KeyError:
                 # return 'The trigger check endpoint is set up wrong.'
                 pass
-        variableType = type(rjson).__name__
-        try:
-            trigger_value = eval(variableType+'("'+trigger_value+'")')
-        except (TypeError, ValueError):
-            pass
+        trigger_value = autoconvert(trigger_value)
         if rjson == trigger_value:
             return '{"loggedIn":true}'
         time.sleep(1)
@@ -171,7 +187,6 @@ def check_for_new():
     original_count = 10000000
     original_count_flag = False
     timer = 0
-    import pdb; pdb.set_trace()
     while timer < 60:
         timer = timer + 1
         # while not trigger:
@@ -186,53 +201,68 @@ def check_for_new():
             trigger = True
             break
         time.sleep(1)
-
     if not trigger:
         return 'TIMEOUT'
 
     # The new thing should be the last in the list
     the_new_thing = rjson.pop()
 
-    def _get_data_at_endpoint(json_data, endpoints):
-        for endpoint in endpoints:
-            json_data = json_data[endpoint]
-        data = json_data # Should be a string or int now, not json
-        return data
-
     # Save the thing_to_remember in the database
-    thing_to_remember = _get_data_at_endpoint(the_new_thing, thing_to_remember_endpoints)
+    thing_to_remember = get_data_at_endpoint(the_new_thing, thing_to_remember_endpoints)
     thing_to_remember_db = Thing_to_remember(access_token,thing_to_remember)
     db.session.add(thing_to_remember_db)
     db.session.commit()
 
     # Return the value at the trigger_value endpoint
-    new_thing_name = _get_data_at_endpoint(the_new_thing, trigger_value_endpoints)
+    new_thing_name = get_data_at_endpoint(the_new_thing, trigger_value_endpoints)
     return '{"newThingName":"'+new_thing_name+'"}'
 
 @app.route('/get_remembered_thing', methods=['POST'])
 def get_remembered_thing():
-    # import pdb; pdb.set_trace()
     access_token = request.args['access_token']
     trigger_endpoint = request.form['triggerEndpoint']
-    thing_to_remember = Thing_to_remember.query.filter_by(access_token=access_token).all()
-    thing_to_remember = thing_to_remember.pop() # Get just the last thing
-    # thing_to_remember = thing_to_remember['thing_to_remember'] # Get just the data we're remembering
-    trigger_endpoint = trigger_endpoint.replace('replace_me',str(thing_to_remember))
     trigger_check_endpoint = request.form['triggerCheck']
+    trigger_value_endpoint = request.form['triggerValue']
+    things_to_remember = Thing_to_remember.query.filter_by(access_token=access_token).all()
+    thing_to_remember = things_to_remember.pop() # Get just the last thing
+    trigger_endpoint = trigger_endpoint.replace('replace_me',str(thing_to_remember))
     counter = 0
-    print trigger_endpoint+access_token
     while counter < 60:
         r = requests.get(trigger_endpoint+access_token)
         rjson = r.json()
-        try:
+        if trigger_check_endpoint in rjson:
+            # if trigger_value_endpoint in rjson:
             new_data = rjson[trigger_check_endpoint]
             return '{"newData":"'+new_data+'"}'
-        except KeyError:
-            print KeyError
-            pass
         counter = counter + 1
         time.sleep(1)
-    return 'TIME OUT'
+    return 'TIMEOUT'
+
+@app.route('/get_added_data', methods=['POST'])
+def get_added_data():
+    # Doesn't actually need to return the photo from FB.
+    access_token = request.args['access_token']
+    trigger_endpoint = request.form['triggerEndpoint']
+    trigger_check_endpoints = request.form['triggerCheck'].split(',')
+    trigger_value = request.form['triggerValue']
+    trigger_value2_endpoints = request.form['thingToRemember'].split(',')
+    things_to_remember = Thing_to_remember.query.filter_by(access_token=access_token).all()
+    thing_to_remember = things_to_remember.pop() # Get just the last thing
+    trigger_endpoint = trigger_endpoint.replace('replace_me',str(thing_to_remember))
+    counter = 0
+    while counter < 60:
+        r = requests.get(trigger_endpoint+access_token)
+        rjson = r.json()
+        # Check if certain endpoint equals something
+        trigger_check = get_data_at_endpoint(rjson, trigger_check_endpoints)
+        trigger_value2 = get_data_at_endpoint(rjson, trigger_value2_endpoints)
+        trigger_value = autoconvert(trigger_value)
+        if trigger_check == trigger_value:
+            new_data = trigger_value2
+            return '{"newData":"'+new_data+'"}'
+        counter = counter + 1
+        time.sleep(1)
+    return 'TIMEOUT'
             
         
 
