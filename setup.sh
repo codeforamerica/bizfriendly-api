bdef178c8dc1cf7a3ae956ff4a99089066bab2c6
Andrew Hyder <hyd415@gmail.com>
2013-09-12 16:55:38 -0700

    Working on testing.

--- setup.sh
+++ setup.sh
@@ -11,4 +11,4 @@ export DATABASE_URL='postgres://hackyourcity@localhost/howtocity'
 
 # Uncomment and set secret key to whatever unique value you want.
 export SECRET_KEY='123456'
-export API_URL='http://app.bizfriend.ly/api'
+export API_URL='http://app.bizfriend.ly'
