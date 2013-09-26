1c89343d021d990081563ef7beca76e4ef6d5f26
Andrew Hyder <hyd415@gmail.com>
2013-09-25 19:35:42 -0700

    Config for staging

--- bizfriendly/config.py
+++ bizfriendly/config.py
@@ -5,9 +5,9 @@ import os
 heroku = Heroku(app) # Sets CONFIG automagically
 app.config.update(
     # DEBUG = True,
-    SQLALCHEMY_DATABASE_URI = 'postgres://hackyourcity@localhost/howtocity',
+    # SQLALCHEMY_DATABASE_URI = 'postgres://hackyourcity@localhost/howtocity',
     # SQLALCHEMY_DATABASE_URI = 'postgres://postgres:root@localhost/howtocity',
-    SECRET_KEY = '123456'
+    # SECRET_KEY = '123456'
 )
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 app.config['MAIL_GUN_KEY'] = os.environ.get('MAIL_GUN_KEY')
